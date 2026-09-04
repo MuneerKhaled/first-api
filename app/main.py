f37c0849ff014a0711339d22e6df3d1a24d147a7
MuneerKhaled <muneerkhaled5@gmail.com>
2026-09-05 02:41:44 +0530

    change

--- app/main.py
+++ app/main.py
@@ -1,7 +1,53 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-print("Sum:", a + b)
-print("Difference:", a - b)
-print("Product:", a * b)
-print("Division:", a / b)
+app = FastAPI(title="Task Manager API")
+
+
+class Task(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+
+tasks = []
+
+
+@app.get("/")
+def home():
+    return {"message": "Task Manager API is running!"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    return {
+        "message": "Task created successfully",
+        "task": task
+    }
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    if task_id >= len(tasks):
+        return {"error": "Task not found"}
+
+    return tasks[task_id]
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    if task_id >= len(tasks):
+        return {"error": "Task not found"}
+
+    deleted_task = tasks.pop(task_id)
+
+    return {
+        "message": "Task deleted successfully",
+        "task": deleted_task
+    }
